dd1965559a64aad4c2f04c22ee42cb58cab31e40
alan <alan7091534@gmail.com>
2024-10-25 16:34:10 +0800

    20241025的改動

---
+++ client/pages/cart/[index copy].js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Beginner from '@/components/homepage/Beginner'
+
+
+function Cart() {
+  return (
+    <>
+      <Beginner />
+      <Beginner />
+      <Beginner />
+      <Beginner />
+    </>
+  )
+}
+
+export default Cart
