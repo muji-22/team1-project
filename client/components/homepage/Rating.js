18688ac7011495e19a6dd69ad5eb30f0a074b031
JZ <sam6032001@gmail.com>
2024-10-28 13:21:53 +0800

    顧客評分樣式修改

--- client/components/homepage/Rating.js
+++ client/components/homepage/Rating.js
@@ -1,74 +1,63 @@
-import React from 'react'
-import styles from './Rating.module.css'
+import React from "react";
+import styles from "./Rating.module.css";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
 
 const Rating = () => {
   const products = [
-    { name: '農場早點名', price: 790, image: '#Image網址.png' },
-    { name: '貓貓食堂', price: 490, image: '#Image網址.png' },
-    { name: '鯊魚警報', price: 490, image: '#Image網址.png' },
-    { name: '呼拉大師', price: 490, image: '#Image網址.png' },
-  ]
+    { name: "農場早點名", price: 790, image: "#Image網址.png" },
+    { name: "貓貓食堂", price: 490, image: "#Image網址.png" },
+    { name: "鯊魚警報", price: 490, image: "#Image網址.png" },
+    { name: "呼拉大師", price: 490, image: "#Image網址.png" },
+  ];
 
   return (
-    <section className={`${styles.section} container py-5`}>
-      <div className="row justify-content-center mb-4">
-        <div className="col-md-8 text-center">
-          <h2 className={`${styles.sectionTitle} mb-3`}>顧客評分</h2>
-          <p className={styles.sectionDescription}>
-            放商品資訊、評分、購買連結等
-          </p>
-        </div>
-      </div>
-      <div className="row justify-content-end mb-4">
-        <div className="col-auto">
-          <button className={`btn ${styles.arrowButton}`}>
-          <FaArrowLeft/>
-          </button>
-          <button className={`btn ${styles.arrowButton}`}>
-          <FaArrowRight/>
-          </button>
-        </div>
-      </div>
+    <section className={`container ${styles.section} py-5`}>
       <div className="row">
         {products.map((product, index) => (
-          <div key={index} className="col-md-6 col-lg-3 mb-4">
-            <div
-              className={`card ${styles.productCard} ${
-                index % 2 === 0 ? styles.evenCard : styles.oddCard
-              }`}
-            >
-              <div className="card-body d-flex justify-content-between align-items-center">
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className={styles.productImage}
-                />
-                <div className={styles.productInfo}>
-                  <h5 className={styles.productName}>{product.name}</h5>
-                  <p className={styles.productPrice}>{product.price}</p>
+          <div key={index} className="col-12 mb-3">
+            <div className="h-100">
+              <div
+                className={`p-3 border rounded-2 ${
+                  index % 2 === 0 ? styles.evenCard : styles.oddCard
+                }`}
+              >
+                <div className="row g-3">
+                  <div className="col-12 col-sm-6">
+                    <div className={styles.productInfo}>
+                      <h5 className={styles.productName}>{product.name}</h5>
+                      <p className={styles.productPrice}>{product.price}</p>
+                    </div>
+                  </div>
+
+                  <div
+                    className={`${styles.ratingContainer} col-12 col-lg-4 col-sm-4 d-flex justify-content-center align-items-center`}
+                  >
+                    {[...Array(5)].map((_, i) => (
+                      <FaStar
+                        key={i}
+                        className={`${styles.star} ${
+                          i < 3 ? styles.filled : ""
+                        } text-custom fs-5 me-1`}
+                      />
+                    ))}
+                  </div>
+
+                  <div className="col-12 col-sm-2 d-flex justify-content-center">
+                    <button className={`btn btn-dark ${styles.buyButton}`}>
+                      立即購買
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div className={`${styles.ratingContainer} my-2`}>
-                {[...Array(5)].map((_, i) => (
-                  <img
-                    key={i}
-                    src="Icon.svg"
-                    alt="Star"
-                    className={styles.starIcon}
-                  />
-                ))}
-              </div>
-              <button className={`btn btn-dark ${styles.buyButton}`}>
-                立即購買
-              </button>
             </div>
           </div>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Rating
+export default Rating;
