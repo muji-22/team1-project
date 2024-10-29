d3a0389c62667131c6e726db1760ddbc2227a1f6
muji-22 <muchiwang608@gmail.com>
2024-10-29 11:13:02 +0800

    回復檔案並更改分類排版

---
+++ client/pages/rentList.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import CategorySidebar from "@/components/product/category";
+import ProductCard from "@/components/product/productCard";
+function HomePage() {
+    return (
+        <div className="container mb-5">
+            <h2 className=" my-5">商品列表</h2>
+
+            {/* 主要內容區 */}
+            <div className="row">
+                {/* 左側篩選欄 */}
+                <div className="col-md-3">
+                    <CategorySidebar />
+                </div>
+
+                {/* 右側內容區 */}
+                <div className="col-md-9">
+                    {/* 這裡可以放置商品列表或其他內容 */}
+                    <div className="p-3">{/* 商品列表內容 */}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
