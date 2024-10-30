7ab09287ab8d1b0b2429938eba7edeb826cf8797
alan <alan7091534@gmail.com>
2024-10-30 16:36:51 +0800

    新增cart的元件、cart的css

--- client/pages/cart/index.js
+++ client/pages/cart/index.js
@@ -1,14 +1,56 @@
-import React from 'react'
-import Beginner from '@/components/homepage/Beginner'
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import StepOne from '@/components/cart/stepone';
+import StepTwo from '@/components/cart/steptwo';
+import StepThree from '@/components/cart/stepthree';
+import styles from '@/styles/cart.module.css';
+
+export default function index() {
 
 
-function Cart() {
+
+  const [cartList, setCartList] = useState([])
+
+
+
+
   return (
     <>
-      <Beginner />
-      <Beginner />
+      <Container fluid= {"xxl"}>
+
+        <Row className={`d-flex justify-content-center align-items-center ${styles.stepBar}`}>
+          <Col className={styles.step}>
+          <div className={styles.stepBox}>
+            <div className={styles.stepNum}>1</div>
+          </div>
+          <div className={styles.stepWord}>
+            <div className={styles.phoneDNone}>第一步</div>
+            <div className={styles.phoneDNone}></div>
+            <span>購物車</span>
+          </div>
+          </Col>
+          <Col className={styles.step}>
+          <div className={styles.stepBox}>
+            <div className={styles.stepNum}>2</div>
+          </div>
+          <div className={styles.stepWord}>
+            <div className={styles.phoneDNone}>第二步</div>
+            <div className={styles.phoneDNone}></div>
+            <span>填寫資料</span>
+          </div>
+          </Col>
+          <Col className={styles.step}>
+          <div className={styles.stepBox}>
+            <div className={styles.stepNum}>3</div>
+          </div>
+          <div className={styles.stepWord}>
+            <div className={styles.phoneDNone}>第三步</div>
+            <div className={styles.phoneDNone}></div>
+            <span>最後確認</span>
+          </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
-
-export default Cart
