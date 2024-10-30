5793292d00b47faf206eaebf0f3b35e7e68b4186
JZ <sam6032001@gmail.com>
2024-10-30 15:23:44 +0800

    後端環境測試

--- server/database/connect.js
+++ server/database/connect.js
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise'
 import 'dotenv/config'
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,  // 使用現有的資料庫名稱
+  host: "127.0.0.1",
+  user: "admin",
+  password: "admin",
+  database: "my_test_db",  // 使用現有的資料庫名稱
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
