79552717c6f23462b52d881a9878137bdbca731e
kuan <crazy85824@yahoo.com.tw>
2024-11-05 14:54:29 +0800

    個人資料修改

--- client/components/users/userData.js
+++ client/components/users/userData.js
@@ -1,20 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./user.module.css";
-import { GiDiceSixFacesOne, GiDiceSixFacesTwo, GiDiceSixFacesThree, GiDiceSixFacesFour, GiDiceSixFacesFive, GiDiceSixFacesSix } from "react-icons/gi";
+import {
+  GiDiceSixFacesOne,
+  GiDiceSixFacesTwo,
+  GiDiceSixFacesThree,
+  GiDiceSixFacesFour,
+  GiDiceSixFacesFive,
+  GiDiceSixFacesSix,
+} from "react-icons/gi";
 import { useAuth } from "@/contexts/AuthContext";
 
-
 export default function UserData() {
-  const {user} = useAuth()
-  const input = [
-    { id: '1', lable: '帳號', icon: <GiDiceSixFacesOne />, type: 'account', orther: 'disabled', },
-    { id: '2', lable: '名稱', icon: <GiDiceSixFacesTwo />, type: 'text', orther: '' },
-    { id: '3', lable: '手機', icon: <GiDiceSixFacesThree />, type: 'phone', orther: '' },
-    { id: '4', lable: '信箱', icon: <GiDiceSixFacesFour />, type: 'email', orther: '' },
-    { id: '5', lable: '地址', icon: <GiDiceSixFacesFive />, type: 'address', orther: '' },
-    { id: '6', lable: '生日', icon: <GiDiceSixFacesSix />, type: 'date', orther: '' }
+  const { user } = useAuth();
+  const [formData, setFormData] = useState({
+    account: user ? user.account : "",
+    name: user ? user.name : "",
+    phone: user ? user.phone : "",
+    email: user ? user.email : "",
+    address: user ? user.address : "",
+    birthday: user ? user.birthday : "",
+  });
 
-  ]
+  const input = [
+    {
+      id: "1",
+      lable: "帳號",
+      icon: <GiDiceSixFacesOne />,
+      type: "account",
+      name: "account",
+      disabled:"disabled"
+    },
+    {
+      id: "2",
+      lable: "名稱",
+      icon: <GiDiceSixFacesTwo />,
+      type: "text",
+      name: "name",
+      disabled:""
+    },
+    {
+      id: "3",
+      lable: "手機",
+      icon: <GiDiceSixFacesThree />,
+      type: "phone",
+      name: "phone",
+    },
+    {
+      id: "4",
+      lable: "信箱",
+      icon: <GiDiceSixFacesFour />,
+      type: "email",
+      name: "email",
+       disabled:""
+    },
+    {
+      id: "5",
+      lable: "地址",
+      icon: <GiDiceSixFacesFive />,
+      type: "address",
+      name: "address",
+       disabled:""
+    },
+    {
+      id: "6",
+      lable: "生日",
+      icon: <GiDiceSixFacesSix />,
+      type: "date",
+      name: "date",
+      disabled:""
+    },
+  ];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <>
       <div className={styles.navSpace} />
@@ -22,13 +86,24 @@ export default function UserData() {
         <div className={styles.reUserLeft}>
           <form action>
             {input.map((v, i) => {
-              return <div className={styles.inputWrap} key={v.id}>
-                <label className={styles.label}>{v.icon}&nbsp;{v.lable}</label>
-                <input className={styles.inputContext} type={v.type} disabled={v.orther} value={0}  />
-              </div>
+              return (
+                <div className={styles.inputWrap} key={v.id}>
+                  <label className={styles.label}>
+                    {v.icon}&nbsp;{v.lable}
+                  </label>
+                  <input
+                    className={styles.inputContext}
+                    type={v.type}
+                    name={v.name}
+                    value={formData[v.name]}
+                    disabled={v.disabled}
+                    onChange={handleChange}
+                  />
+                </div>
+              );
             })}
             <button className={styles.button} type="submit">
-              修改{!user?'0':user.name}
+              修改
             </button>
           </form>
         </div>
