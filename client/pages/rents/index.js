740afd1eb1238854969201a486eb5c8b5dd01aea
JZ <sam6032001@gmail.com>
2024-11-12 09:23:30 +0800

    bugfix

--- client/pages/rents/index.js
+++ client/pages/rents/index.js
@@ -129,7 +129,7 @@ function Rents() {
       <div className="d-lg-none mb-4">
         <div className="d-flex justify-content-end gap-2 overflow-auto pb-2">
           <button
-            className="btn btn-primary d-flex align-items-center gap-2 flex-shrink-0"
+            className="btn btn-custom d-flex align-items-center gap-2 flex-shrink-0 rounded-pill"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#filterOffcanvasBottom"
