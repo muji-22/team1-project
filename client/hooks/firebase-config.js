cd1548c48a146e96496af8c6462b87732ef6e25f
JZ <sam6032001@gmail.com>
2024-11-14 12:23:30 +0800

    google登入...testing

---
+++ client/hooks/firebase-config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDLuWoychMlsju0TuMcdXp6KJTrv9-WJwI",
+    authDomain: "pertho-4722f.firebaseapp.com",
+    projectId: "pertho-4722f",
+    storageBucket: "pertho-4722f.firebasestorage.app",
+    messagingSenderId: "272578407598",
+    appId: "1:272578407598:web:cc23436f97cd96d2ec513f"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
