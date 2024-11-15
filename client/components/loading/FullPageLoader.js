25960fe218e8c4de926d415f2961fd3e7a543634
JinZhi <sam6032001@gmail.com>
2024-11-15 15:11:37 +0800

    樣式修改

--- client/components/loading/FullPageLoader.js
+++ client/components/loading/FullPageLoader.js
@@ -5,8 +5,8 @@ const FullPageLoader = () => {
   return (
     <>
       <div
-        className="vh-100 vw-100 d-flex flex-column justify-content-center align-items-center position-fixed top-0 start-0 bg-light"
-        style={{ zIndex: 9999, backgroundColor: "rgba(255, 255, 255, 0.8)" }}
+        className="vh-100 vw-100 d-flex flex-column justify-content-center align-items-center position-fixed top-0 start-0 bg-white"
+        style={{ zIndex: 9999, opacity: 0.9, backdropFilter: "blur(10px)" }}
       >
         <DiceComponent className="mb-5" />
         <PropagateLoader color="#40CBCE" size={20} speedMultiplier={1} />
