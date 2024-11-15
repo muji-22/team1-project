72b088a8e6567ebb534a66ae27d981d72b25332c
JinZhi <sam6032001@gmail.com>
2024-11-15 11:28:14 +0800

    修改loading動畫

--- client/components/loading/FullPageLoader.js
+++ client/components/loading/FullPageLoader.js
@@ -1,19 +1,18 @@
-import { PulseLoader, BarLoader } from 'react-spinners'
-import DiceComponent from '@/components/loading/DiceComponent'
+import { PropagateLoader } from "react-spinners";
+import DiceComponent from "@/components/loading/DiceComponent";
 
 const FullPageLoader = () => {
   return (
     <>
-    <div className="vh-100 vw-100 d-flex justify-content-center align-items-center position-fixed top-0 start-0 bg-light" style={{ zIndex: 9999 }}>
-      <PulseLoader color="#36d7b7" />
-      <DiceComponent />
-    </div>
-      <BarLoader
-  height={10}
-  width={500}
-/>
-</>
-  )
-}
+      <div
+        className="vh-100 vw-100 d-flex flex-column justify-content-center align-items-center position-fixed top-0 start-0 bg-light"
+        style={{ zIndex: 9999, backgroundColor: "rgba(255, 255, 255, 0.8)" }}
+      >
+        <DiceComponent className="mb-5" />
+        <PropagateLoader color="#40CBCE" size={20} speedMultiplier={1} />
+      </div>
+    </>
+  );
+};
 
-export default FullPageLoader
+export default FullPageLoader;
