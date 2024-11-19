8ad04dc60ba100f1fa91d60d93ce98bf55061141
JZ <sam6032001@gmail.com>
2024-11-19 15:23:41 +0800

    增加title

--- client/pages/auth/user.js
+++ client/pages/auth/user.js
@@ -1,5 +1,6 @@
 // pages/auth/user.js
 import React from "react";
+import Head from "next/head";
 import UserFrom from "@/components/users/userFrom";
 
 
@@ -7,6 +8,9 @@ import UserFrom from "@/components/users/userFrom";
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>會員中心 | Pertho</title>
+      </Head>
       <UserFrom />
     </>
   );
